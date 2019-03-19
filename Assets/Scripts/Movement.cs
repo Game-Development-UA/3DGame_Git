f1a33350d94ff65ca7d60fcbad5cfd4e5e6adc5a
Alex <alozano101@gmail.com>
2019-03-18 21:07:55 -0700

    Added movement script and demo object

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float horizonatal_mvmt;
+    float vertical_mvmt;
+    public float jump;
+    public float speed;
+    public Rigidbody charac;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizonatal_mvmt = Input.GetAxis("Horizontal");
+        vertical_mvmt = Input.GetAxis("Vertical");
+        charac.velocity = new Vector2(speed * horizonatal_mvmt, charac.velocity.y);
+    }
+}
