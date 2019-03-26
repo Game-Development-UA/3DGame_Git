c94e1e02a07f0dba1044d94aeb7675308bb44ba2
Alex <alozano101@gmail.com>
2019-03-25 20:53:41 -0700

    Added sprint feature

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,10 +8,13 @@ public class Movement : MonoBehaviour
     float vertical_mvmt;
     public float jump;
     public float speed;
+    public float RunSpeed;
     public Rigidbody charac;
-    public Projectile projectilePrefab;
-    public int maxProjectiles;
-    public List<Projectile> projectiles = new List<Projectile>();
+    float timeLeft = 3.0f; // Change for longer sprint
+
+    //public Projectile projectilePrefab;
+    //public int maxProjectiles;
+    //public List<Projectile> projectiles = new List<Projectile>();
 
     // Start is called before the first frame update
     void Start()
@@ -31,8 +34,24 @@ public class Movement : MonoBehaviour
         horizonatal_mvmt = Input.GetAxis("Horizontal");
         vertical_mvmt = Input.GetAxis("Vertical");
         charac.velocity = new Vector3(speed * horizonatal_mvmt, charac.velocity.y, vertical_mvmt * speed);
-    }
 
+        //Sprint feature
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed += RunSpeed;
+            charac.velocity = new Vector3(speed * horizonatal_mvmt, charac.velocity.y, vertical_mvmt * speed);
+        }
+        //Timer for Sprint
+        timeLeft -= Time.deltaTime;
+        if(timeLeft < 0 && timeLeft >-0.01)
+        {
+            speed -= RunSpeed;
+            charac.velocity = new Vector3(speed * horizonatal_mvmt, charac.velocity.y, vertical_mvmt * speed);
+    
+        timeLeft = 3.0f;
+        }
+        
+    }
     public void Jump()
     {
         if (Input.GetButtonDown("Jump"))
@@ -40,8 +59,8 @@ public class Movement : MonoBehaviour
             charac.AddForce(new Vector3(0f, jump * Time.deltaTime, 0f), ForceMode.Impulse);
         }
     }
-    public void ProjectileDestroyed(Projectile projectileThatWasDestroyed)
-    {
-        projectiles.Remove(projectileThatWasDestroyed);
-    }
+    //public void ProjectileDestroyed(Projectile projectileThatWasDestroyed)
+    //{
+        //projectiles.Remove(projectileThatWasDestroyed);
+    //}
 }
