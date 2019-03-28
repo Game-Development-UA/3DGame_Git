bf2c726465280d91b005c9c03b5226c4e12aa7ce
Shannon Washington <blendergirl12@gmail.com>
2019-03-28 10:15:54 -0700

    Dog Bone Rotations and Texture

--- Assets/Scripts/DogBoneRotate.cs
+++ Assets/Scripts/DogBoneRotate.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class DogBoneRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public int rotations = 30;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0f, 0f, rotations*Time.deltaTime);
     }
 }
