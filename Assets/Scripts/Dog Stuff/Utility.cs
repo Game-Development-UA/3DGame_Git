c30bfd1db9f777048527ca7e48b6621dfabbb911
Alex <alozano101@gmail.com>
2019-04-22 13:45:56 -0700

    Added a new option for utility for dog abilities

---
+++ Assets/Scripts/Dog Stuff/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "My New Utility Ability", menuName = "Dog Abilities/Utility Ability")]
+public class Utility : Dog_Abilities
+{
+    public int distance;
+  
+    public override void Cast(GameObject target)
+    {
+
+    }
+}
