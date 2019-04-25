0d283500719a1519154a0b26cde331ecddad3231
shannonwashington <blendergirl12@gmail.com>
2019-04-24 19:24:38 -0700

    Add files via upload
    not working on GitKraken

---
+++ Scripts/DogBoneRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogBoneRotate : MonoBehaviour
+{
+    public int rotations = 30;
+    // Update is called once per frame
+    public float x;
+    public float y;
+    public float z;
+    public Vector3 pos;
+
+    public void Start()
+    {
+        //Random Spawn
+        x = UnityEngine.Random.Range(-50, 50);
+        y = 0;
+        z = UnityEngine.Random.Range(-50, 50);
+        pos = new Vector3(x, y, z);
+        transform.position = pos;
+    }
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, rotations*Time.deltaTime);        
+    }
+}
---
+++ Scripts/SimpleController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleController : MonoBehaviour
+{
+	public int maxProjectiles;
+
+	public Transform projectileSpawnLocation;
+
+	public Projectile projectilePrefab;
+
+	public List<Projectile> projectiles = new List<Projectile>();
+    //public Transform dogPosition;
+
+    public int counter = 0;
+
+
+    void Update() {
+        if (counter == 5)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("WinPage");
+        }
+
+        if ( Input.GetButtonDown( "Fire1" ) ) {
+			if( projectiles.Count < maxProjectiles ) {
+				Projectile newProjectile = Instantiate<Projectile>( projectilePrefab);
+				newProjectile.transform.position = projectileSpawnLocation.position;
+				newProjectile.transform.forward = projectileSpawnLocation.right;
+
+				newProjectile.playerOwner = this;
+
+				projectiles.Add( newProjectile );
+			} 
+		}
+
+	}
+
+
+	public void ProjectileDestroyed( Projectile projectileThatWasDestroyed ) {
+        counter += 1;
+        projectiles.Remove( projectileThatWasDestroyed );
+	}
+
+    public void ProjectileDestroyedTime(Projectile projectileThatWasDestroyed)
+    {
+        //counter += 1;
+        projectiles.Remove(projectileThatWasDestroyed);
+    }
+
+    /*void OnGUI()
+    {
+        GUI.Label(new Rect(100, 0, 100, 50), "Score:  " + counter);
+
+    }*/
+}
